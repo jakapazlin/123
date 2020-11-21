64d258a737bcc03fb65684259b8c8ad3886b9e21
jakapazlin <jpazlin13@gmail.com>
2020-11-22 00:46:37 +0700

    jaka

---
+++ configg.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "YWU2ZDAzMDctN2JiMS00ZjE2LThjNTItZWIwNDZiZmVmZjQ4";
+$uuid = "ddbed9c2-9606-464e-a6a2-7157dce10831";
+$userid = "34915121";
+
+$eksternal_version = "2.0.2";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0.1; vivo 1610) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.120 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
